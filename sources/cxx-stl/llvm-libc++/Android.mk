32751fc0605ad324fca7a5f44355fa95fcd9524e
Dan Albert <danalbert@google.com>
2016-06-14 16:53:18 -0700

    Remove libc++_shared's DT_NEEDED on libstdc++.so.
    Backport of https://android-review.googlesource.com/#/c/239232/
    
    Bug: https://github.com/android-ndk/ndk/issues/105
    Change-Id: Iacbd0e58c07ee9d40e3411912ae15b61ec1c2f50

--- sources/cxx-stl/llvm-libc++/Android.mk
+++ sources/cxx-stl/llvm-libc++/Android.mk
@@ -119,6 +119,10 @@ LOCAL_EXPORT_CPPFLAGS := $(libcxx_export_cxxflags)
 LOCAL_EXPORT_LDFLAGS := $(libcxx_export_ldflags)
 LOCAL_STATIC_LIBRARIES := libc++abi android_support
 LOCAL_LDFLAGS := $(libcxx_ldflags)
+# Use --as-needed to strip the DT_NEEDED on libstdc++.so (bionic's) that the
+# driver always links for C++ but we don't use.
+# See https://github.com/android-ndk/ndk/issues/105
+LOCAL_LDFLAGS += -Wl,--as-needed
 
 # We use the LLVM unwinder for all the 32-bit ARM targets.
 ifneq (,$(filter armeabi%,$(TARGET_ARCH_ABI)))
