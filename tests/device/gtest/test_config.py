8135c2821d0b014d19f37207267ffa6cb1e97d8d
Ryan Prichard <rprichard@google.com>
2018-09-14 23:24:55 -0700

    Disable googletest-death-test-test on pre-O
    death-test-test fails on pre-O because raise() doesn't reliably report
    a killed-by-signal status to the parent. AFAIK, googletest's death
    tests are still reliable pre-O as long as we don't rely on raise() to
    kill the child process.
    
    Bug: https://github.com/android-ndk/ndk/issues/795
    Test: ./checkbuild.py && ./run_tests.py
    Change-Id: I67f22459835c843c6d767af5319b48d0c71817a0

--- tests/device/gtest/test_config.py
+++ tests/device/gtest/test_config.py
@@ -1,3 +1,10 @@
+def run_unsupported(abi, device_api, name):
+    # The tested behavior fails reliably on API 16, but it's flaky on 24, so
+    # skip the test until 26 where it appears reliable.
+    if name == 'googletest-death-test-test' and device_api < 26:
+        return 'android-{} (https://github.com/android-ndk/ndk/issues/795)'.format(device_api)
+
+
 def run_broken(_abi, device_api, name):
     if name == 'googletest-printers-test' and device_api <= 16:
         return ('android-{}'.format(device_api),
