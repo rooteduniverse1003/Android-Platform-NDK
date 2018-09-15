f7d11efa4bf19a01de7dc788ddfc1232cb509d3a
Ryan Prichard <rprichard@google.com>
2018-09-14 23:23:22 -0700

    Update gtest config for renamed test
    This test was recently renamed[1], but it's still failing on an
    android-16 device.
    
    [1] commit 1f96f40514e54269e8262a8c52beaf3a1800d550
    
    Bug: none
    Test: ./checkbuild.py && ./run_tests.py
    Change-Id: I0dc819374e0a27b94466a588e3ff1cb6c25b5da7

--- tests/device/gtest/test_config.py
+++ tests/device/gtest/test_config.py
@@ -1,5 +1,5 @@
 def run_broken(_abi, device_api, name):
-    if name == 'gtest-printers_test' and device_api <= 16:
+    if name == 'googletest-printers-test' and device_api <= 16:
         return ('android-{}'.format(device_api),
                 'https://github.com/android-ndk/ndk/issues/771')
     return None, None
