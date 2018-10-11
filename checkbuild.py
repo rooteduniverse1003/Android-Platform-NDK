2bf39e21d6347580cac0f0d902aa881e7e822ece
Dan Albert <danalbert@google.com>
2018-10-11 13:25:38 -0700

    Update to clang r339409b.
    Test: ./checkbuild.py && ./run_tests.py
    Bug: None
    Change-Id: Ie84481b9bbc2d7b5483b7850e72aab752c2a0a01

--- checkbuild.py
+++ checkbuild.py
@@ -223,7 +223,7 @@ def _install_file(src_file, dst_file):
 class Clang(ndk.builds.Module):
     name = 'clang'
     path = 'toolchains/llvm/prebuilt/{host}'
-    version = 'clang-r339409'
+    version = 'clang-r339409b'
     notice_group = ndk.builds.NoticeGroup.TOOLCHAIN
 
     @property
