e817b92faf6b0648f370542aeb93e7e704b69732
Christopher Ferris <cferris@google.com>
2019-06-06 18:23:00 -0700

    Add support for apk parsing.
    Changes:
    - Update to verify build id information if present.
    - Add support for handling the XXX.apk!libXX.so format.
    - Add support for handling XXX.apk.
    - Add unittests.
    - Add a system test.
    
    Bug: 131748083
    
    Test: Ran new unit tests.
    Test: Ran system test.
    Test: Verified on mac/linux/windows with a stack trace that inludes
    Test: all different kinds of stack (with apk, with build id, with
    Test: mismatched build id, etc).
    Test: Verified on linux running from a standalone install.
    Change-Id: I04c3c406309014131cc81b72a98185e703accee7

---
+++ tests/ndk-stack/ndk_stack_systemtest.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""System tests for ndk-stack.py"""
+
+from __future__ import print_function
+
+from io import StringIO
+import os.path
+import sys
+import unittest
+from unittest.mock import patch
+
+sys.path.insert(0, '../..')
+ndk_stack = __import__('ndk-stack')
+
+import ndk.hosts  # pylint:disable=wrong-import-position
+import ndk.toolchains  # pylint:disable=wrong-import-position
+
+
+class SystemTests(unittest.TestCase):
+    """Complete system test of ndk-stack.py script."""
+
+    def setUp(self):
+        default_host = ndk.hosts.get_default_host()
+        clang_toolchain = ndk.toolchains.ClangToolchain(default_host)
+
+        # First try and use the normal functions, and if they fail, then
+        # use hard-coded paths from the development locations.
+        ndk_paths = ndk_stack.get_ndk_paths()
+        self.readelf = ndk_stack.find_readelf(*ndk_paths)
+        if not self.readelf:
+            self.readelf = clang_toolchain.gcc_toolchain.gcc_tool('readelf')
+        self.assertTrue(self.readelf)
+        self.assertTrue(os.path.exists(self.readelf))
+
+        try:
+            self.llvm_symbolizer = ndk_stack.find_llvm_symbolizer(*ndk_paths)
+        except OSError:
+            self.llvm_symbolizer = str(
+                clang_toolchain.clang_tool('llvm-symbolizer'))
+        self.assertTrue(self.llvm_symbolizer)
+        self.assertTrue(os.path.exists(self.llvm_symbolizer))
+
+    @patch.object(ndk_stack, 'find_llvm_symbolizer')
+    @patch.object(ndk_stack, 'find_readelf')
+    def test_all_stacks(self, mock_readelf, mock_llvm_symbolizer):
+        mock_readelf.return_value = self.readelf
+        mock_llvm_symbolizer.return_value = self.llvm_symbolizer
+
+        symbol_dir = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), 'files')
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            ndk_stack.main([
+                '-s', symbol_dir, '-i',
+                os.path.join(symbol_dir, 'backtrace.txt')
+            ])
+
+        # Read the expected output.
+        file_name = os.path.join(symbol_dir, 'expected.txt')
+        with open(mode='r', file=file_name) as exp_file:
+            expected = exp_file.read()
+        expected = expected.replace('SYMBOL_DIR', symbol_dir)
+        self.maxDiff = None
+        self.assertEqual(expected, mock_stdout.getvalue())
+
+
+if __name__ == '__main__':
+    unittest.main()
