f91c9a5b9a5202b733f1336baffe7224b0d1b76a
Dan Albert <danalbert@google.com>
2019-10-10 14:36:26 -0700

    Update test config for updated fortify support.
    Test: ./run_tests.py --rebuild
    Bug: None
    Change-Id: Iabe3a6e69ab3488faf8dcc29051abb0fba6326e0

--- tests/build/fortify_strcpy_available/test_config.py
+++ tests/build/fortify_strcpy_available/test_config.py
@@ -3,11 +3,5 @@ from typing import Optional
 from ndk.test.types import Test
 
 
-def build_unsupported(test: Test) -> Optional[str]:
-    if test.config.api is not None and test.config.api < 17:
-        return f'__strcpy_chk not available in android-{test.config.api}'
-    return None
-
-
 def is_negative_test() -> bool:
     return True
