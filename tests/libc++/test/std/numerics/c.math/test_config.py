615bac21b7fc3849fc0eaca17c4849be24448ec6
Dan Albert <danalbert@google.com>
2020-04-15 16:17:44 -0700

    Mark abs.fail.cpp broken for pre-API 19.
    Switching to using Clang's `-verify` feature for testing the compile
    failure tests means that we're now checking that all the *correct*
    diagnostics are firing, rather than just compilation failing. The
    -Wabsolute-value warning expected here doesn't fire for pre-API 19
    (see the bug for more details).
    
    Test: ./run_tests.py --rebuild --filter libc++
    Bug: https://github.com/android/ndk/issues/1237
    Change-Id: I4482823d28a7c38d3e29c040450b7741f697aa60

--- tests/libc++/test/std/numerics/c.math/test_config.py
+++ tests/libc++/test/std/numerics/c.math/test_config.py
@@ -3,4 +3,7 @@ def build_broken(test):
         return 'all', 'http://b/34724220'
     if test.case_name == 'cmath_isinf.pass' and test.config.api >= 21:
         return f'android-{test.config.api}', 'http://b/34724220'
+    if test.case_name == 'abs.fail' and test.config.api < 19:
+        bug = 'https://github.com/android/ndk/issues/1237'
+        return f'android-{test.config.api}', bug
     return None, None
