42d1392997d6bd14a74b1556da5c9aa29e4beeb5
Dan Albert <danalbert@google.com>
2021-11-30 11:50:50 -0800

    Mark another test broken.
    Same as the prior commit, just the other flavor of this test.
    
    Test: ./run_tests.py --rebuild (but without a filter this time)
    Bug: https://github.com/android/ndk/issues/1108
    Change-Id: I69e2d4d4f0a7255c8d14ca85681764c14732a71e

--- tests/libc++/test/std/depr/depr.c.headers/test_config.py
+++ tests/libc++/test/std/depr/depr.c.headers/test_config.py
@@ -3,4 +3,6 @@ def build_broken(test):
         return 'all', 'http://b/34724220'
     if test.case_name == 'math_h_isinf.pass' and test.config.api >= 21:
         return f'android-{test.config.api}', 'http://b/34724220'
+    if test.case_name == 'stdlib_h.pass' and test.config.api < 21:
+        return 'pre API 21', 'https://github.com/android/ndk/issues/1108'
     return None, None
--- tests/libc++/test/std/language.support/support.runtime/test_config.py
+++ tests/libc++/test/std/language.support/support.runtime/test_config.py
@@ -5,6 +5,6 @@ from ndk.test.types import Test
 
 def build_broken(test: Test) -> tuple[Optional[str], Optional[str]]:
     if test.case_name == 'cstdlib.pass' and test.config.api < 21:
-        return 'all', 'https://github.com/android/ndk/issues/1108'
+        return 'pre API 21', 'https://github.com/android/ndk/issues/1108'
     return None, None
 
