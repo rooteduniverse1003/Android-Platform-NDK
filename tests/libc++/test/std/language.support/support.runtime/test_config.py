bfffcf943cf36e07b16bffaf520d27ee69bcf8b9
Dan Albert <danalbert@google.com>
2022-01-12 09:20:43 -0800

    Revert "Mark another test broken."
    No longer needed.
    
    This reverts commit 11ccda700e2b385c4aebe497ec7a47ed2e599ad1.
    
    Bug: https://github.com/android/ndk/issues/1108
    Test: Treehugger
    Change-Id: Iaa6442765815645aa73173e8cf2ecebc737bd9c6

--- tests/libc++/test/std/language.support/support.runtime/test_config.py
+++ tests/libc++/test/std/language.support/support.runtime/test_config.py
@@ -5,5 +5,5 @@ from ndk.test.types import Test
 
 def build_broken(test: Test) -> tuple[Optional[str], Optional[str]]:
     if test.case_name == "cstdlib.pass" and test.config.api < 21:
-        return "pre API 21", "https://github.com/android/ndk/issues/1108"
+        return "all", "https://github.com/android/ndk/issues/1108"
     return None, None
