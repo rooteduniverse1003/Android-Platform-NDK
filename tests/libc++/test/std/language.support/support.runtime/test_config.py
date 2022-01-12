fdb5304040b7309683a6e875bd6529384c546e53
Dan Albert <danalbert@google.com>
2022-01-12 09:21:34 -0800

    Revert "Mark cstdlib test as broken."
    No longer needed.
    
    This reverts commit 42fb40db52fe46cfb2b62f94163fd3a3d29e717a.
    
    Bug: https://github.com/android/ndk/issues/1108
    Test: Treehugger
    Change-Id: I25d6650eaf6228588ad488f7115c5e961b9289b7

--- tests/libc++/test/std/language.support/support.runtime/test_config.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from ndk.test.types import Test
-
-
-def build_broken(test: Test) -> tuple[Optional[str], Optional[str]]:
-    if test.case_name == "cstdlib.pass" and test.config.api < 21:
-        return "all", "https://github.com/android/ndk/issues/1108"
-    return None, None
