512a73fb2049c74361821f3292076bc8b4b0bb44
Dan Albert <danalbert@google.com>
2022-08-19 14:08:00 -0700

    Test on API 21 devices.
    To prepare for dropping kitkat support, make sure our tests are all
    behaving on API 21.
    
    With the exception of ASan, all the changes here were to fix test bugs,
    not actual bugs. ASan still needs to be investigated, but I suspect that
    one is also a test bug (missing LD_PRELOAD).
    
    Bug: https://github.com/android/ndk/issues/1751
    Test: treehugger
    Change-Id: I59462c8d046b4aac17fd36f5ab1831867ff02cbe

--- tests/device/asan-smoke/test_config.py
+++ tests/device/asan-smoke/test_config.py
@@ -1,8 +1,20 @@
-def run_unsupported(test, device):
+from optparse import Option
+from typing import Optional
+from ndk.test.devices import Device
+from ndk.test.devicetest.case import TestCase
+
+
+def run_unsupported(test: TestCase, device: Device) -> Optional[str]:
     if device.version < 19:
-        return device.version
+        return f"{device.version}"
     if device.version >= 28 and test.config.abi == "x86_64":
         # ASAN is flaky with 28 x86_64. It still works with 32-bit or with
         # older platforms.
         return "ASAN is flaky on 28 x86_64 (http://b/37130178)"
     return None
+
+
+def run_broken(test: TestCase, device: Device) -> tuple[Optional[str], Optional[str]]:
+    if device.version == 21:
+        return f"{device.version}", "https://github.com/android/ndk/issues/1753"
+    return None, None
