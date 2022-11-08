6ec890af258fd90661635e3255af1dd805784c1b
zijunzhao <zijunzhao@google.com>
2022-11-08 23:40:49 +0000

    Reformat via pylint and black
    Bug: None
    Tests: None
    Change-Id: I5e9770bd067707efbcf8b303ae3cf5a5471e1931

--- tests/device/weak_symbols/test_config.py
+++ tests/device/weak_symbols/test_config.py
@@ -1,5 +1,6 @@
 from ndk.test.spec import WeakSymbolsConfig
 
+
 def build_unsupported(test) -> bool:
     # skip this test to avoid redefining __ANDROID_UNAVAILABLE_SYMBOLS_ARE_WEAK__
     if test.config.weak_symbol == WeakSymbolsConfig.WeakAPI:
