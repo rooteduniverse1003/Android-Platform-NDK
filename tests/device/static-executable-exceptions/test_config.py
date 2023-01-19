7896ec066089438cded7fe3faf1a13332521cc5a
Dan Albert <danalbert@google.com>
2023-01-19 15:23:50 -0800

    Python 3.9 compat.
    Not needed in the main branch, but we need this for r25.
    
    Bug: None
    Test: ./checkbuild.py pythonlint
    Change-Id: If9739d221f594c917ab4e08271adaa5ebfee809d
    (cherry picked from commit e2bd297ecef4974ad65ca35b0f306e1815e16578)
    Merged-In: If9739d221f594c917ab4e08271adaa5ebfee809d

--- tests/device/static-executable-exceptions/test_config.py
+++ tests/device/static-executable-exceptions/test_config.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import ndk.abis
 from ndk.test.buildtest.case import Test
 
@@ -7,7 +9,7 @@ def extra_cmake_flags() -> list[str]:
     return ["-DANDROID_PLATFORM=latest"]
 
 
-def override_runtime_minsdkversion(test: Test) -> int | None:
+def override_runtime_minsdkversion(test: Test) -> Union[int, None]:
     # We build as latest because static executables require that, but static executables
     # are compatible with old OS versions.
     return ndk.abis.min_api_for_abi(test.config.abi)
