87def9745a06a73ea137fa6c4c7b475bdcc4f8d1
Dan Albert <danalbert@google.com>
2023-05-10 11:20:55 -0700

    Convert ndk-gdb to a PEP-441 zip application.
    Python supports executing zipped applications and running them in that
    manner makes dependency discovery easier, since the application
    directory will automatically be added to sys.path.
    
    We do have to stop using hyphens in our script names though, because
    those aren't valid python modules.
    
    This does potentially allow us to do more cool things. If we moved
    ndkgdb.py into ndk.tools, we could include the rest of the ndk package
    so we can use things like ndk.hosts and ndk.abis. Moving it there also
    makes it easier to ensure that it'll be covered by our linters and
    tests, which was the real motivation for this change.
    
    Bug: None
    Test: ./checkbuild.py ndk-gdb ndk-gdb-shortcut && $NDK/ndk-gdb --help
    Change-Id: Ibdd0c2d42a2eccaf46d188221421601f740b02d9

--- ndk/checkbuild.py
+++ ndk/checkbuild.py
@@ -1726,18 +1726,18 @@ class Changelog(ndk.builds.FileModule):
 
 
 @register
-class NdkGdb(ndk.builds.MultiFileModule):
+class NdkGdb(ndk.builds.PythonApplication):
     name = "ndk-gdb"
-    install_path = Path("prebuilt/{host}/bin")
+    install_path = Path("prebuilt/{host}/bin/ndkgdb.pyz")
     notice = NDK_DIR / "NOTICE"
-
-    @property
-    def files(self) -> Iterator[Path]:
-        yield NDK_DIR / "ndk-gdb"
-        yield NDK_DIR / "ndk-gdb.py"
-
-        if self.host.is_windows:
-            yield NDK_DIR / "ndk-gdb.cmd"
+    package = NDK_DIR / "ndkgdb.py"
+    main = "ndkgdb:main"
+    pip_dependencies = [
+        ANDROID_DIR / "development/python-packages/adb",
+    ]
+    copy_to_python_path = [
+        ANDROID_DIR / "development/python-packages/gdbrunner",
+    ]
 
 
 @register
