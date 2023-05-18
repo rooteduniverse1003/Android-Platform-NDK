5e4952baeefb95db9e67d1b20a30a4c32c912596
Dan Albert <danalbert@google.com>
2023-05-18 16:17:54 -0700

    Remove some obsolete tests.
    Unsupported ABIs and API levels.
    
    Bug: None
    Test: None
    Change-Id: I60825e9ed5619060e918c249a0c23547e024070d

--- tests/build/issue21132-__ARM_ARCH__/test_config.py
+++
@@ -1,4 +0,0 @@
-def build_unsupported(test):
-    if test.config.abi not in ("armeabi-v7a", "x86"):
-        return test.config.abi
-    return None
--- tests/device/clone/test_config.py
+++
@@ -1,4 +0,0 @@
-def build_unsupported(test):
-    if test.config.abi == "x86" and test.config.api < 17:
-        return test.config.abi
-    return None
