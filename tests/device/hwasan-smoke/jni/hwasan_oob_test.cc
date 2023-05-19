d1ab36ea78a09aa28185eba425f4ba6155c5e852
Florian Mayer <fmayer@google.com>
2023-05-18 17:28:22 -0700

    Add tests for hwasan binaries in NDK
    Bug: 276930343
    Test: ./run_tests.py --filter hwasan-smoke --rebuild
          with manually edited qc_config.json to cover API 34.
    Change-Id: Idb376c38f16b38e75d7a4978871d5b71bce7acaa

---
+++ tests/device/hwasan-smoke/jni/hwasan_oob_test.cc
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#if !defined(__aarch64__)
+#error "HWASan is only supported on AArch64."
+#endif
+
+TEST(HWAddressSanitizer, OOB) {
+  EXPECT_DEATH({
+      volatile char* x = const_cast<volatile char*>(reinterpret_cast<char*>(malloc(1)));
+      x[1] = '2';
+      }, ".*HWAddressSanitizer.*");
+}
