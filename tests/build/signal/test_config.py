bc4948823d65173f80d53bd871ba5b9af4d97750
Prashanth Swaminathan <prashanthsw@google.com>
2023-07-13 17:46:44 -0700

    Disable signal test for 'riscv64'
    As explained in https://github.com/android/ndk/issues/160, the
    bsd_signal call is only present on android-21, so architectures that
    have a minimum SDK version above android-21 should be excluded.
    
    Test: checkbuild.py
    Change-Id: I78b58f8dc8e3646d9a8e65ab8b16845ed1414400

--- tests/build/signal/test_config.py
+++ tests/build/signal/test_config.py
@@ -1,4 +1,4 @@
 def build_unsupported(test):
-    if test.config.abi in ("arm64-v8a", "x86_64"):
+    if test.config.abi in ("arm64-v8a", "riscv64", "x86_64"):
         return test.config.abi
     return None
