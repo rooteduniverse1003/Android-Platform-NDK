e32a94f33de7aca7cf6f8f519740c73793c7c12c
Dan Albert <danalbert@google.com>
2023-09-29 14:41:59 -0700

    Revert "Mark gtest-port-test broken for API 34+."
    https://github.com/google/googletest/pull/4334 changed the test to not
    rely on this behavior (I'm pretty sure it was wrong to do that, because
    that's a breaking change for gtest, which now has inconsistent regex
    behavior). Android got an update yesterday, and it landed interleaved
    with my change that added the xfail, so this is now passing again.
    
    This reverts commit 04092c7e38d4f00e69e96da367f73fe1c2996da8.
    
    Bug: https://github.com/android/ndk/issues/1944
    Test: ./run_tests.py
    Change-Id: Ic98c7f89acebc498b9178822e3c5cd2127e40603

--- tests/device/gtest/test_config.py
+++ tests/device/gtest/test_config.py
@@ -1,19 +1,7 @@
-from ndk.test.devices import Device
-from ndk.test.devicetest.case import BasicTestCase
-
-
-def run_unsupported(test: BasicTestCase, device: Device) -> str | None:
+def run_unsupported(test, device):
     # The tested behavior fails reliably on API 16, but it's flaky on 24, so
     # skip the test until 26 where it appears reliable.
     if test.executable == "googletest-death-test-test" and device.version < 26:
         bug = "https://github.com/android-ndk/ndk/issues/795"
         return f"android-{device.version} ({bug})"
     return None
-
-
-def run_broken(
-    test: BasicTestCase, device: Device
-) -> tuple[str, str] | tuple[None, None]:
-    if test.executable == "googletest-port-test" and device.version >= 34:
-        return f"android-{device.version}", "https://github.com/android/ndk/issues/1944"
-    return None, None
